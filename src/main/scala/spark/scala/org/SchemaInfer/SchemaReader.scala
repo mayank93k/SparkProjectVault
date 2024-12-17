2f7cc1caa569aec5d78442c2e949dcef0add055d
mayank93k <mayank93.k@gmail.com>
2024-12-18 00:43:26 +0530

    updated Repo

---
+++ src/main/scala/spark/scala/org/SchemaInfer/SchemaReader.scala
@@ -0,0 +1,31 @@
+package spark.scala.org.SchemaInfer
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.catalyst.ScalaReflection
+import org.apache.spark.sql.types._
+
+import scala.reflect.runtime.universe._
+
+object SchemaReader {
+  val spark = SparkSession.builder().master("local").getOrCreate()
+  val readFile = spark.read.schema(inferJsonSchema)
+
+  /**
+   * Utility to infer schema for different source system
+   *
+   * @return Struct Typesafe config
+   */
+  private def inferJsonSchema(): StructType = {
+    readSchema[readInput]
+  }
+
+  /**
+   * Utility to read schema from T type case class
+   *
+   * @tparam T : T type case class
+   * @return
+   */
+  private def readSchema[T <: Product : TypeTag]: StructType = ScalaReflection.schemaFor[T].dataType.asInstanceOf[StructType]
+
+  private case class readInput(name: String, age: Int)
+}

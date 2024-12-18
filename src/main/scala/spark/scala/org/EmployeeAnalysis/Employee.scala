6753342cbfa43e5d688846a8e4862ef7e9a6178d
mayank93k <mayank93.k@gmail.com>
2024-12-18 11:53:30 +0530

    Refactor

--- src/main/scala/spark/scala/org/EmployeeAnalysis/Employee.scala
+++
@@ -1,5 +0,0 @@
-//package spark.scala.org.EmployeeDataAnalysis
-//
-//case class Employee(Name: String, Department: String, Salary: Double, City: String, State: String, Idno: Int, phoneNo: String, Country: String, Designation: String, Machine: String)
-//
-//

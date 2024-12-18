4dcf20aac8b8093a353ad8ce14420efa376d0a23
mayank93k <mayank93.k@gmail.com>
2024-12-18 10:57:21 +0530

    Refactor

--- src/main/scala/spark/scala/org/travelanalysis/TravelAnalysis.scala
+++ src/main/scala/spark/scala/org/travelanalysis/TravelAnalysis.scala
@@ -1,22 +1,21 @@
-package spark.scala.org.TravelAnalysis
+package spark.scala.org.travelanalysis
 
-import org.apache.log4j._
 import org.apache.spark._
-import spark.scala.org.generic.InputOutputFileUtility
+import spark.scala.org.common.logger.Logging
 
 //create class "TravelAnalysis1"
-class TravelAnalysis1(sc: SparkContext) {
+class TravelAnalysis(sc: SparkContext) extends Logging {
 
   //create function destination to calculate the top 20 destination people travel the most
-  def destination(t: String): Unit = {
+  def destination(): Unit = {
 
     //Input dataset in txt format
-    val travel = sc.textFile(InputOutputFileUtility.getInputPath(t))
+    val travel = sc.textFile("src/main/resources/input/travelanalysis/TravelData.txt")
 
     //******************
-    //Part1: Top 20 destination people travel the most
+    // Part1: Top 20 destination people travel the most
     //******************
-
+    logger.info("Part1: Top 20 destination people travel the most")
     val split = travel.map(lines => lines.split("\t")) //Splitting the txt file using "\t" which is tab delimited
     val count = split.map(x => (x(2), 1)).reduceByKey(_ + _)
     val sort = count.map(item => item.swap).sortByKey(ascending = false)
@@ -29,15 +28,15 @@ class TravelAnalysis1(sc: SparkContext) {
   }
 
   //create function location to calculate top 20 location from people travel the most
-  def location(t: String): Unit = {
+  def location(): Unit = {
 
     //Input dataset in txt format
-    val travel = sc.textFile(InputOutputFileUtility.getInputPath(t))
+    val travel = sc.textFile("src/main/resources/input/travelanalysis/TravelData.txt")
 
     //**********************
-    //Part2: Top 20 location from people travel the most
+    // Part2: Top 20 location from people travel the most
     //**********************
-
+    logger.info("Part2: Top 20 location from people travel the most")
     val split = travel.map(lines => lines.split("\t")) //Splitting the txt file using "\t" which is tab delimited
     val count = split.map(x => (x(1), 1)).reduceByKey(_ + _)
     val sort = count.map(item => item.swap).sortByKey(ascending = false)
@@ -50,15 +49,15 @@ class TravelAnalysis1(sc: SparkContext) {
   }
 
   //create function revenue to calculate the cities that generate high airline revenues for travel
-  def revenue(t: String): Unit = {
+  def revenue(): Unit = {
 
     //Input dataset in txt format
-    val travel = sc.textFile(InputOutputFileUtility.getInputPath(t))
+    val travel = sc.textFile("src/main/resources/input/travelanalysis/TravelData.txt")
 
     //**********************
-    //Part3 cities that generate high airline revenues for travel
+    // Part3 cities that generate high airline revenues for travel
     //**********************
-
+    logger.info("Part3 cities that generate high airline revenues for travel")
     val split = travel.map(lines => lines.split("\t")) //Splitting the txt file using "\t" which is tab delimited
     val filter = split.filter(x => {
       if (x(3).matches("1")) true else false
@@ -74,15 +73,15 @@ class TravelAnalysis1(sc: SparkContext) {
   }
 
   //create function adults to calculate the no of adults traveling is greater than or equal to 2 and less than equal to 4
-  def adults(t: String): Unit = {
+  def adults(): Unit = {
 
     //Input dataset in txt format
-    val travel = sc.textFile(InputOutputFileUtility.getInputPath(t))
+    val travel = sc.textFile("src/main/resources/input/travelanalysis/TravelData.txt")
 
     //**************************
-    //Part4 No of adults traveling is greater than or equal to 2 and less than equal to 4 
+    // Part4 No of adults traveling is greater than or equal to 2 and less than equal to 4
     //**************************
-
+    logger.info("Part4 No of adults traveling is greater than or equal to 2 and less than equal to 4")
     val split = travel.map(lines => lines.split("\t")) //Splitting the txt file using "\t" which is tab delimited
     val filter = split.filter(x => {
       if (x(4) >= "2" && x(4) <= "4") true else false
@@ -92,19 +91,16 @@ class TravelAnalysis1(sc: SparkContext) {
   }
 }
 
-object TravelAnalysis1 {
-  System.setProperty("hadoop.home.dir", "C:\\winutils")
-
+object TravelAnalysis {
   def main(args: Array[String]): Unit = {
-    Logger.getLogger("org").setLevel(Level.ERROR)
     //Creation of SparkContext object
-    val sc = new SparkContext("local[*]", "TravelAnalysis")
+    val sc = new SparkContext("local[*]", "travelanalysis")
 
     //object creation for class TravelAnalysis1
-    val job = new TravelAnalysis1(sc)
-    job.destination("TravelData.txt")
-    job.location("TravelData.txt")
-    job.revenue("TravelData.txt")
-    job.adults("TravelData.txt")
+    val job = new TravelAnalysis(sc)
+    job.destination()
+    job.location()
+    job.revenue()
+    job.adults()
   }
 }
